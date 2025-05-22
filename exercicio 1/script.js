3ab1895d13c6c7b8624d2d7d026abe05ca3f30b9
chuparola2 <142441782+Lucas53635274@users.noreply.github.com>
2025-05-22 17:47:33 -0300

    Add files via upload

---
+++ exercicio 1/script.js
@@ -0,0 +1,12 @@
+let num1
+let num2
+let resultado
+
+function somar(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número"));
+    let num2 = Number(window.prompt("Digite o segundo número"));
+
+    let resultado = num1 + num2;     
+
+    alert("O resultado é " + resultado + ".");
+}
