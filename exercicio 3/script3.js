0174b8e6880b5249b4339b165bb042eb0bddcf41
chuparola2 <142441782+Lucas53635274@users.noreply.github.com>
2025-05-27 12:23:57 -0300

    Update script3.js

--- exercicio 3/script3.js
+++ exercicio 3/script3.js
@@ -1,18 +1,16 @@
-function somar(){
+var nota1 = Number(window.prompt("Digite a primeira nota: "));
+var nota2 = Number(window.prompt("Digite a segunda nota: "));
+var nota3 = Number(window.prompt("Digite a terceira nota: "));
 
-var nota1 = Number(window.prompt("Digite aqui o primeiro nota"));
-var nota2 = Number(window.prompt("Digite o segundo nota"));
-var nota3 = Number(window.prompt("Digite a terceira nota"));
+let total = (nota1 + nota2 +nota3) / 3;
 
-let total = (nota1 + nota2 + nota3 ) / 6;
 
-}
+alert ("media final: " + total.toFixed(1) + ".")
 
-alert("media final: " +total)
-
-if(total >=60){
-      alert("vc atingiu a media" + total)
-      alert("aprovado")
-}else{
-    pon
-}
+if(total >= 60){ 
+    alert("Você atingiu a media!! " + total.toFixed(1))
+    alert (" Aprovado")
+} else { 
+    let pontosFaltando = 60 - total
+    alert("Você foi reprovado, faltaram" + pontosFaltando.toFixed(1) + " pontos")
+}   
