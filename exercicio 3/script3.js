f2735ef471f45718c9b3c9f936a9968868402acc
chuparola2 <142441782+Lucas53635274@users.noreply.github.com>
2025-05-27 08:48:04 -0300

    Add files via upload

--- exercicio 3/script3.js
+++ exercicio 3/script3.js
@@ -1,20 +1,18 @@
 function somar(){
-let nota1;
-let nota2;
-let nota3;
-let resultado;
 
-nota1 = Number(window.prompt("Digite aqui o primeiro nota"));
-nota2 = Number(window.prompt("Digite o segundo nota"));
-nota3 = Number(window.prompt("Digite a terceira nota"));
+var nota1 = Number(window.prompt("Digite aqui o primeiro nota"));
+var nota2 = Number(window.prompt("Digite o segundo nota"));
+var nota3 = Number(window.prompt("Digite a terceira nota"));
 
-resultado = nota1 + nota2 + nota3 / 6;
+let total = (nota1 + nota2 + nota3 ) / 6;
 
 }
 
-if (resultado => 180) {
-    alert("Você passou!" + resultado + ".");
-} else {
-    if (resultado < 180)
-        alert ("Você não passou!" + resultado + ".")
+alert("media final: " +total)
+
+if(total >=60){
+      alert("vc atingiu a media" + total)
+      alert("aprovado")
+}else{
+    pon
 }
