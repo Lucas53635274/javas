62fee1848d000b32438bd5db5a6bf501444900a4
chuparola2 <142441782+Lucas53635274@users.noreply.github.com>
2025-06-10 12:27:37 -0300

    Add files via upload

---
+++ exercicio 6/script6.js
@@ -0,0 +1,17 @@
+let cliques  = 0;
+let mostraHTML = document.getElementById("mostraHTML");
+function contador() {
+
+    cliques++;
+
+    mostraHTML.innerText = `Número de cliques : ${cliques}`;
+    //console.log(cliques);
+
+}
+
+
+
+
+ 
+
+
