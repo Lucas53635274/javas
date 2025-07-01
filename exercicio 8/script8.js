2167252bef3e652a8778a4e82b62e63f974f6c05
chuparola2 <142441782+Lucas53635274@users.noreply.github.com>
2025-07-01 10:36:55 -0300

    Update script8.js

--- exercicio 8/script8.js
+++ exercicio 8/script8.js
@@ -1,15 +1,22 @@
 let mostraHTML = document.getElementById("mostraHTML");
-
 let numero = 0;
-let chance
-let acerto
+let num_adiv = 0;
 
-function aleatorio(){
+numero = parseInt(Math.random() * 101)
 
-    numero = parseInt (Math.random() * 101);
-    
-    mostraHTML.innerHTML += `<p>Pensei em um número acerte-o:</p>`;
+function adiv(){
+    num_adiv = Number(window.prompt("Digite um número de 1 a 100"));
 
-    console.log(aleatorio);
+    frase = mostraHTML.innerHTML += `<p>Você escolheu ${num_adiv}.</p>`;
+
+    if(num_adiv > numero) {
+        mostraHTML.innerHTML = `Você escolheu ${num_adiv}. Meu número é MENOR.</p>`;
+
+    } else {
+        mostraHTML.innerHTML = `Você escolheu ${num_adiv}. Meu número é MAIOR.</p>`;
+    }
+
+    if(numero == num_adiv) {
+        mostraHTML.innerHTML = `<p>Parabéns você acertou!`
+    }
 }
-
